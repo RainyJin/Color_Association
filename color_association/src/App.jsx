d268522467c33bb67c15025dba7e4705762d654f
RainyJin <rainyjin2016@gmail.com>
2024-06-04 15:08:20 -0400

    Fixed some bugs

--- color_association/src/App.jsx
+++ color_association/src/App.jsx
@@ -1,30 +1,32 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import Create from './components/Create'
-import ListColors from './components/ListColors';
-import { Toaster } from 'react-hot-toast';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Create from "./components/Create";
+import ListColors from "./components/ListColors";
+import { Toaster } from "react-hot-toast";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 function App() {
-  const [colors, setColors] = useState([]);
+  const [colors, setColors] = useState(["#990000", "#009900"]);
 
-  console.log(colors)
+  console.log(colors);
 
   useEffect(() => {
     setColors(JSON.parse(localStorage.getItem("colors")));
-  }, [])
+  }, []);
 
   return (
     <DndProvider backend={HTML5Backend}>
       <Toaster />
-      <div className="bg-slate-100 w-screen h-screen flex flex-col
-        items-center p-32 gap-16 pt-32">
+      <div
+        className="bg-slate-100 w-screen h-screen flex flex-col
+        items-center p-32 gap-16 pt-32"
+      >
         <Create colors={colors} setColors={setColors} />
         <ListColors colors={colors} setColors={setColors} />
       </div>
     </DndProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- color_association/src/components/Create.jsx
+++ color_association/src/components/Create.jsx
@@ -1,53 +1,65 @@
 import { useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
-import toast, { Toaster } from 'react-hot-toast';
+import { v4 as uuidv4 } from "uuid";
+import toast, { Toaster } from "react-hot-toast";
 
-const Create = ({ colors, setColors }) => {
+const Create = ({ colors = [], setColors }) => {
   const [color, setColor] = useState({
-    id: "",
+    id: uuidv4(),
     name: "",
-    status: "created" //can also be of the 4 given selections
+    status: "created",
   });
-
-  //console.log(color);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (color.name.length != 7) return toast.error("Color must be rgb 6 characters ie. '000000'");
+    if (color.name.length !== 7 || !/^#[0-9A-Fa-f]{6}$/.test(color.name)) {
+      return toast.error(
+        "Color must be a valid hex code of 6 characters, e.g., '#000000'"
+      );
+    }
 
-    setColors((prev) => {
-      const list = [...prev, color];
-
+    setColors((prevColors) => {
+      const validPrevColors = Array.isArray(prevColors) ? prevColors : [];
+      const list = [...validPrevColors, color];
       localStorage.setItem("colors", JSON.stringify(list));
-
       return list;
     });
 
     toast.success("Color created");
 
     setColor({
-      id: "",
+      id: uuidv4(),
       name: "",
-      status: "created" //can also be deleted
+      status: "created",
     });
+  };
 
-  }
+  const handleInputChange = (e) => {
+    setColor((prevColor) => ({
+      ...prevColor,
+      name: "#" + e.target.value,
+    }));
+  };
 
-  return <>
-    <Toaster />
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text"
-          className="border-2 border-slate-400 bg-slate-100 rounded-md mr-4 h-12
-          w-64 px-1"
-          onChange={(e) => setColor({ ...color, id: uuidv4(), name: "#" + e.target.value })} />
-        <button className="bg-gray-500 rounded-md px-4 h-12 text-white">
-          Create
-        </button>
-      </form>
-    </div>
-  </>
-}
+  return (
+    <>
+      <Toaster />
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="border-2 border-slate-400 bg-slate-100 rounded-md mr-4 h-12 w-64 px-1"
+            onChange={handleInputChange}
+            value={color.name.slice(1)} // Remove '#' for display in the input field
+            placeholder="Enter hex color code"
+          />
+          <button className="bg-gray-500 rounded-md px-4 h-12 text-white">
+            Create
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default Create;
